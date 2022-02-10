b070d8e9c488635536fb6c1533d4e4d8d51fa67d
Cathy Bastareaud <catybastareaud@gmail.com>
2022-02-10 15:16:40 -0700

    Updating fragment to use spinners

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -16,6 +17,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import com.google.android.material.snackbar.Snackbar;
 import edu.cnm.deepdive.codebreaker.R;
+import edu.cnm.deepdive.codebreaker.adapter.CodeCharacterAdapter;
 import edu.cnm.deepdive.codebreaker.databinding.FragmentGameBinding;
 import edu.cnm.deepdive.codebreaker.model.entity.Guess;
 import edu.cnm.deepdive.codebreaker.viewmodel.GameViewModel;
@@ -28,7 +30,7 @@ public class GameFragment extends Fragment {
   private FragmentGameBinding binding;
   private Map<Character, Integer> colorValueMap;
   private Map<Character, String> colorLabelMap;
-  private Character[]  poolCharacters;
+  private Character[] poolCharacters;
   private Spinner[] spinners;
   private int codeLength;
 
@@ -42,13 +44,7 @@ public class GameFragment extends Fragment {
       ViewGroup container, Bundle savedInstanceState) {
 
     binding = FragmentGameBinding.inflate(inflater, container, false);
-    binding.submit.setOnClickListener((view) -> {
-      String text = binding.guess
-          .getText()
-          .toString()
-          .trim();
-      gameViewModel.submitGuess(text);
-    });
+
     setupMaps();
 
     return binding.getRoot();
@@ -62,9 +58,9 @@ public class GameFragment extends Fragment {
     gameViewModel
         .getThrowable()
         .observe(getViewLifecycleOwner(), (throwable) -> {
-          if (throwable!= null) {
+          if (throwable != null) {
             Snackbar
-                .make(binding.getRoot(),throwable.getMessage(),Snackbar.LENGTH_LONG)
+                .make(binding.getRoot(), throwable.getMessage(), Snackbar.LENGTH_LONG)
                 .show();
           }
 
@@ -72,31 +68,32 @@ public class GameFragment extends Fragment {
         });
     gameViewModel
         .getGame()
-        .observe(getViewLifecycleOwner(), (game )-> {
-          binding.guess.setText("");
+        .observe(getViewLifecycleOwner(), (game) -> {
+          codeLength = game.getLength();
           ArrayAdapter<Guess> adapter = new ArrayAdapter<>(getContext(),
-                  android.R.layout.simple_list_item_1,game.getGuesses());
+              android.R.layout.simple_list_item_1, game.getGuesses());
           binding.guesses.setAdapter(adapter);
-          binding.submit.setEnabled( ! game.isSolved());
+          binding.submit.setEnabled(!game.isSolved());
+          // TODO : Hide/show binding.guessControls.
         });
   }
 
   @Override
   public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
     super.onCreateOptionsMenu(menu, inflater);
-    inflater.inflate(R.menu.game_options,menu);
+    inflater.inflate(R.menu.game_options, menu);
 
   }
 
   @Override
   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
     boolean handled;
-    if (item.getItemId()== R.id.new_game) {
+    if (item.getItemId() == R.id.new_game) {
 
       gameViewModel.startGame("ABCDEF", 3);
       handled = true;
     } else {
-        handled =super.onOptionsItemSelected(item);
+      handled = super.onOptionsItemSelected(item);
 
     }
     return handled;
@@ -107,16 +104,17 @@ public class GameFragment extends Fragment {
     super.onDestroyView();
     binding = null;
   }
-  private void setupMaps(){
+
+  private void setupMaps() {
     String colors = getString(R.string.color_chars);
     char[] colorChars = colors.toCharArray();
     poolCharacters = colors
         .chars()
-        .mapToObj((value)->(char) value)
+        .mapToObj((value) -> (char) value)
         .toArray(Character[]::new);
-    Resources resources =getResources();
+    Resources resources = getResources();
     int[] colorValues = resources.getIntArray(R.array.color_values);
-    String[] colorLabels= resources.getStringArray(R.array.color_labels);
+    String[] colorLabels = resources.getStringArray(R.array.color_labels);
     colorValueMap = buildValueMap(colorChars, colorValues);
     colorLabelMap = buildLabelMap(colorChars, colorLabels);
 
@@ -124,9 +122,9 @@ public class GameFragment extends Fragment {
   }
 
   private static Map<Character, Integer> buildValueMap(char[] chars, int[] values) {
-    Map<Character, Integer> valueMap=new HashMap<>();
-    for (int i = 0; i< chars.length; i++) {
-     valueMap.put(chars[i], values[i]);
+    Map<Character, Integer> valueMap = new HashMap<>();
+    for (int i = 0; i < chars.length; i++) {
+      valueMap.put(chars[i], values[i]);
 
     }
 
@@ -136,10 +134,39 @@ public class GameFragment extends Fragment {
 
   private static Map<Character, String> buildLabelMap(char[] chars, String[] labels) {
     Map<Character, String> labelMap = new HashMap<>();
-    for (int i = 0; i< chars.length; i++) {
+    for (int i = 0; i < chars.length; i++) {
       labelMap.put(chars[i], labels[i]);
 
     }
-    return labelMap ;
+    return labelMap;
+  }
+
+  private void setupSpinners() {
+    int maxCodeLength = getResources().getInteger(R.integer.code_length_pref_max);
+    LayoutInflater inflater = LayoutInflater.from(getContext());
+    spinners = new Spinner[maxCodeLength];
+    for (int i = 0; i < spinners.length; i++) {
+      Spinner spinner = (Spinner) inflater.inflate(R.layout.swatch_spinner, binding.spinners,
+          false);
+      CodeCharacterAdapter adapter =
+          new CodeCharacterAdapter(getContext(), colorValueMap, colorLabelMap, poolCharacters);
+      spinner.setAdapter(adapter);
+      spinners[i] = spinner;
+      binding.spinners.addView(spinner);
+
+    }
+
+  }
+
+  private void setupListeners() {
+    binding.submit.setOnClickListener((view) -> submitGuess());
+  }
+
+  private void submitGuess() {
+    StringBuilder builder = new StringBuilder(codeLength);
+    for (int i = 0; i < codeLength; i++) {
+      builder.append(spinners[i].getSelectedItem());
+    }
+    gameViewModel.submitGuess(builder.toString());
   }
 }
